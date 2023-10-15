fa7c14913615ab908e668e8af8dcbda9a891649a
EvoltPratom <bishantad@gmail.com>
2023-10-15 12:10:06 +0545

    help strings

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from howto import help_strings
 
 def test1():
     print("Test1. It works")
@@ -13,39 +14,39 @@ parser = argparse.ArgumentParser(description="PyUtils")
 subparsers = parser.add_subparsers(dest="function_name", help="Choose a function to run")
 
 # Subparser for Organize
-parser_function1 = subparsers.add_parser("organize", help="Organize folder according to a config")
+parser_function1 = subparsers.add_parser("organize", help=help_strings["organize"])
 parser_function1.add_argument("--path", type=str, default=os.getcwd(), help="folder_path")
 parser_function1.add_argument("--config", default=None, help="Config defaults to config.json")
 
 # Subparser for flatten
-parser_function2 = subparsers.add_parser("flatten", help="flattens a folder and it's content")
+parser_function2 = subparsers.add_parser("flatten", help=help_strings["flatten"])
 parser_function2.add_argument("--path", type=str, default=os.getcwd(), help="folder_path")
 
 # Subparser for img2pdf
-parser_function3 = subparsers.add_parser("img2pdf", help="Images from a folder to PDF")
+parser_function3 = subparsers.add_parser("img2pdf", help=help_strings["img2pdf"])
 parser_function3.add_argument("--path", type=str, default=os.getcwd(), help="folder_path")
 parser_function3.add_argument("--output", type=str, default=None, help="output PDF")
 parser_function3.add_argument("--dimg", type=bool, default=False, help="Delete Images")
 
 # Subparser for pdf2img
-parser_function3 = subparsers.add_parser("pdf2img", help="PDF to Images")
+parser_function3 = subparsers.add_parser("pdf2img", help=help_strings["pdf2img"])
 parser_function3.add_argument("path", type=str, help="pdf_path")
 parser_function3.add_argument("--output", type=str, default=os.getcwd(), help="output path")
 
 # Subparser for convert_music
-parser_function3 = subparsers.add_parser("convert_music", help="Bulk Convert videos or audio to audio")
+parser_function3 = subparsers.add_parser("convert_music", help=help_strings["convert_music"])
 parser_function3.add_argument("--path", type=str, default=None ,help="audio folder")
 parser_function3.add_argument("--format", type=str, default=None, help="output format")
 parser_function3.add_argument("--daudio", type=bool, default=False, help="Delete originals")
 
 #Subparser for yt mp3 download
-parser_function4 = subparsers.add_parser("ytmp3", help="Yt vdo download -> mp3")
+parser_function4 = subparsers.add_parser("ytmp3", help=help_strings["ytmp3"])
 parser_function4.add_argument("uri", type=str, help="yt uri")
 parser_function4.add_argument("--title", type=str, default=None, help="title in mp4 format")
 
 
 #Subparser for mp4tomp3 download
-parser_function5 = subparsers.add_parser("mp4tomp3", help="Convert single video to audio")
+parser_function5 = subparsers.add_parser("mp4tomp3", help=help_strings["mp4tomp3"])
 parser_function5.add_argument("path", type=str, help="Path of the video")
 
 #Subparser for test download
@@ -53,7 +54,7 @@ parser_function6 = subparsers.add_parser("test1", help="Yess")
 parser_function6.add_argument("--path", default="sd", type=str, help="test")
 
 #subparser for yt playlist
-parser_function7 = subparsers.add_parser("yt_playlist_mp3", help="Download yt playlist as mp3 on current directory")
+parser_function7 = subparsers.add_parser("yt_playlist_mp3", help=help_strings["yt_playlist_mp3"])
 parser_function7.add_argument("uri", type=str, help="playlist url")
 
 
---
+++ howto.py
@@ -0,0 +1,111 @@
+help_strings = {
+    "organize": '''
+    Organize a folder according to a configuration file.
+
+    Usage: python Utility organize [--path <folder_path>] [--config <config_file>]
+
+    Examples:
+      python Utility organize --path \path\to\folder\ --config "my_config.json"
+      (if you want to organize current folder) python Utility organize
+    Options:
+      --path    Folder path to be organized (default: current working directory).
+      --config  Configuration file to use (default: config.json).
+    ''',
+
+    "flatten": '''
+    Flatten a folder and its contents.
+
+    Usage: python Utility flatten [--path <folder_path>]
+
+    Examples:
+      python Utility flatten --path \path\to\folder
+      (if you want to organize current folder) python Utility flatten
+    Options:
+      --path  Folder path to be flattened (default: current working directory).
+    ''',
+
+    "img2pdf": '''
+    Convert images from a folder to a PDF.
+
+    Usage: python Utility img2pdf [--path <folder_path>] [--output <output_PDF>] [--dimg]
+
+    Examples:
+      python Utility img2pdf --path \path\to\images --output output.pdf --dimg 1
+    Options:
+      --path    Folder path containing images (default: current working directory).
+      --output  Output PDF file (default: None).
+      --dimg    Delete source images after conversion (default: False).
+    ''',
+
+    "pdf2img": '''
+    Convert a PDF to images.
+
+    Usage: python Utility pdf2img <pdf_path> [--output <output_path>]
+
+    Examples:
+      python Utility pdf2img \path\to\my_document.pdf --output /output/folder
+    Arguments:
+      pdf_path  Path to the PDF file to be converted to images.
+
+    Options:
+      --output  Output path for the images (default: current working directory).
+    ''',
+
+    "convert_music": '''
+    Bulk convert videos or audio to audio format.
+
+    Usage: python Utility convert_music [--path <audio_folder>] [--format <output_format>] [--daudio]
+
+    Examples:
+      python Utility convert_music --path \path\to\audio_files --format mp3 --daudio 1
+    Options:
+      --path     Folder containing audio or video files (default: None).
+      --format   Output audio format (default: None).
+      --daudio   Delete original files after conversion (default: False).
+    ''',
+
+    "ytmp3": '''
+    Download a YouTube video and convert it to MP3 format.
+
+    Usage: python Utility ytmp3 <youtube_uri> [--title <output_title>]
+
+    Examples:
+      python Utility ytmp3 https://www.youtube.com/watch?v=your_video_id --title "my_audio"
+    Arguments:
+      youtube_uri  YouTube video URL.
+      title       Output title in MP3 format (default: None).
+
+    Options:
+      --title  Specify the output title in MP3 format (default: None).
+    ''',
+
+    "mp4tomp3": '''
+    Convert a single video to audio (MP3 format).
+
+    Usage: python Utility mp4tomp3 <video_path>
+
+    Examples:
+      python Utility mp4tomp3 path\\to\\video.mp4
+    Arguments:
+      video_path  Path to the video file to be converted to audio.
+    ''',
+
+    "test1": '''
+    A test function.
+
+    Usage: python Utility test1 [--path <test_path>]
+    Options:
+      --path  Test path (default: "sd").
+    ''',
+
+    "yt_playlist_mp3": '''
+    Download a YouTube playlist and convert its videos to MP3 format.
+
+    Usage: python Utility yt_playlist_mp3 <playlist_url>
+
+    Examples:
+      python Utility yt_playlist_mp3 https://www.youtube.com/playlist?list=XXXXXXXX
+    Arguments:
+      playlist_url  URL of the YouTube playlist to download.
+    ''',
+}
